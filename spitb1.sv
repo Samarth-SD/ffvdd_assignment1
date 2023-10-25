329ab7be593deebb264d3f5e963894cc5ab1a888
Samarth-SD <97584579+Samarth-SD@users.noreply.github.com>
2023-10-25 18:34:10 +0530

    Update spitb1.sv

--- spitb1.sv
+++ spitb1.sv
@@ -11,7 +11,7 @@ module tb_spi_ctrl;
     wire spi_enb;
     wire spi_di;
 
-    // Instantiate the spi_ctrl module
+    
     spi_ctrl #(
         .SPI_ADDR_WIDTH(16),
         .SPI_CMD_WIDTH(24),
@@ -30,12 +30,12 @@ module tb_spi_ctrl;
         .spi_do(spi_do)
     );
 
-    // Clock generation
+
     always begin
         #5 clk = ~clk;
     end
 
-    // Initial values
+   
     initial begin
         clk = 0;
         rst = 0;
@@ -44,8 +44,7 @@ module tb_spi_ctrl;
         ready = 0;
         spi_do = 0;
 
-        // Test Case 1: IDLE to WRITE_ADDR
-        // This test case goes from IDLE to WRITE_ADDR state
+      
         $display("Test Case 1: IDLE to WRITE_ADDR");
         rst = 1;
         #10 rst = 0;
@@ -55,8 +54,7 @@ module tb_spi_ctrl;
         #100;
         en = 0;
 
-        // Test Case 2: IDLE to WRITE_DATA
-        // This test case goes from IDLE to WRITE_DATA state
+      
         $display("Test Case 2: IDLE to WRITE_DATA");
         cmd_data = 24'b110110110110110110110110;
         en = 1;
@@ -64,8 +62,7 @@ module tb_spi_ctrl;
         #100;
         en = 0;
 
-        // Test Case 3: IDLE to READ
-        // This test case goes from IDLE to READ state
+  
         $display("Test Case 3: IDLE to READ");
         cmd_data = 24'b111111111111111111111111;
         en = 1;
@@ -73,8 +70,7 @@ module tb_spi_ctrl;
         #100;
         en = 0;
 
-        // Test Case 4: WRITE_ADDR to WRITE_DATA
-        // This test case goes from WRITE_ADDR to WRITE_DATA state
+      
         $display("Test Case 4: WRITE_ADDR to WRITE_DATA");
         cmd_data = 24'b101010101010101010101010;
         en = 1;
@@ -86,8 +82,7 @@ module tb_spi_ctrl;
         #50;
         en = 0;
 
-        // Test Case 5: WRITE_DATA to IDLE
-        // This test case goes from WRITE_DATA to IDLE state
+      
         $display("Test Case 5: WRITE_DATA to IDLE");
         cmd_data = 24'b110110110110110110110110;
         en = 1;
@@ -95,8 +90,7 @@ module tb_spi_ctrl;
         #100;
         en = 0;
 
-        // Test Case 6: READ to IDLE
-        // This test case goes from READ to IDLE state
+      
         $display("Test Case 6: READ to IDLE");
         cmd_data = 24'b111111111111111111111111;
         en = 1;
