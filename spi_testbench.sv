5952829e7660262dbfb687793fdfca74ad1f7690
Samarth-SD <97584579+Samarth-SD@users.noreply.github.com>
2023-10-26 16:07:52 +0530

    Update spi_testbench.sv

--- spi_testbench.sv
+++ spi_testbench.sv
@@ -2,106 +2,91 @@ module tb_spi_ctrl;
     reg clk;
     reg rst;
     reg [23:0] cmd_data;
+    wire [7:0] read_data;
     reg en;
     reg ready;
-    wire [7:0] read_data;
     wire sink_vld;
-    reg spi_do;
     wire spi_clk;
     wire spi_enb;
     wire spi_di;
+    reg spi_do;
 
-    
-    spi_ctrl #(
-        .SPI_ADDR_WIDTH(16),
-        .SPI_CMD_WIDTH(24),
-        .SPI_IDLE(0)
-    ) uut (
-        .clk(clk),
-        .rst(rst),
-        .cmd_data(cmd_data),
-        .en(en),
-        .ready(ready),
-        .read_data(read_data),
-        .sink_vld(sink_vld),
-        .spi_clk(spi_clk),
-        .spi_enb(spi_enb),
-        .spi_di(spi_di),
+   
+    spi_ctrl uut (
+        .clk(clk), 
+        .rst(rst), 
+        .cmd_data(cmd_data), 
+        .read_data(read_data), 
+        .en(en), 
+        .ready(ready), 
+        .sink_vld(sink_vld), 
+        .spi_clk(spi_clk), 
+        .spi_enb(spi_enb), 
+        .spi_di(spi_di), 
         .spi_do(spi_do)
     );
 
-
-    always begin
-        #5 clk = ~clk;
-    end
-
-   
     initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars;
+
+        
         clk = 0;
-        rst = 0;
-        cmd_data = 24'b0;
+        rst = 1;
+        cmd_data = 24'h0;
         en = 0;
         ready = 0;
         spi_do = 0;
 
-      
-        $display("Test Case 1: IDLE to WRITE_ADDR");
+        
+        #10;
+
+        
+        rst = 0;
+        en = 1;
+        cmd_data = 24'h123456;
+
+        repeat (100) @(negedge clk);
         rst = 1;
-        #10 rst = 0;
-        cmd_data = 24'b101010101010101010101010;
-        en = 1;
-        ready = 1;
-        #100;
         en = 0;
-
-      
-        $display("Test Case 2: IDLE to WRITE_DATA");
-        cmd_data = 24'b110110110110110110110110;
+        repeat (1) @(negedge clk);
+        rst = 0;
         en = 1;
-        ready = 1;
-        #100;
+        cmd_data = ~cmd_data;
+
+        repeat (100) @(negedge clk);
+        rst = 1;
         en = 0;
-
-  
-        $display("Test Case 3: IDLE to READ");
-        cmd_data = 24'b111111111111111111111111;
+        repeat (1) @(negedge clk);
+        rst = 0;
         en = 1;
-        ready = 1;
-        #100;
+        cmd_data = ~cmd_data;
+
+        repeat (100) @(negedge clk);
+        rst = 1;
         en = 0;
-
-      
-        $display("Test Case 4: WRITE_ADDR to WRITE_DATA");
-        cmd_data = 24'b101010101010101010101010;
+        repeat (1) @(negedge clk);
+        rst = 0;
         en = 1;
-        ready = 1;
-        #50;
-        ready = 0;
-        #50;
-        ready = 1;
-        #50;
-        en = 0;
+        cmd_data = ~cmd_data;
 
-      
-        $display("Test Case 5: WRITE_DATA to IDLE");
-        cmd_data = 24'b110110110110110110110110;
-        en = 1;
-        ready = 1;
-        #100;
-        en = 0;
-
-      
-        $display("Test Case 6: READ to IDLE");
-        cmd_data = 24'b111111111111111111111111;
-        en = 1;
-        ready = 1;
-        #50;
-        ready = 0;
-        #50;
-        ready = 1;
-        #50;
+        repeat (100) @(negedge clk);
+        rst = 1;
         en = 0;
 
         $finish;
     end
+
+    always @(clk) begin
+        if (clk) begin
+            
+            spi_do = $random & 1'b1;
+        end
+        if (~clk) begin
+            
+            ready = $random & 1'b1; 
+        end
+    end
+
+    always #10 clk = ~clk;
 endmodule
